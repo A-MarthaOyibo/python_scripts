65ef3af6292863b51d33af116cd95fc3d85015f0
A-MarthaOyibo <oyibomartha@gmail.com>
2019-03-30 17:05:57 -0600

    refined script for import as a module

--- ip_validity _checker.py
+++ ip_validity _checker.py
@@ -1,26 +1,26 @@
 
-
 #!/usr/bin/env python3
 
-#author : Afure Martha, Oyibo maoy4508@colorado.edu
-#purpose: IP VALIDITY CHECKER
-#date   : 2019.3.03
-#python version: 3.5.2
+'''
+author : Afure Martha, Oyibo
+purpose: IP VALIDITY CHECKER
+date   : 2019.3.03
+python version: 3.5.2
 
-#Note: import as validateIP.verifyIP(device_login_file)
+To import module:
+     from ip_validity_checker import verifyIpOctet
+'''
+#!/usr/bin/env python3
+
+import sys
 
 
-import json,sys
-#file = "sshinfo.json"
-
-def verifyOctet(a):
-    b = a.split(".")
+def verifyIpOctet(ip_addr):
+    b = ip_addr.split(".")
     if len(b) == 4:
-        # print(a)
         for i in b:
             try:
                 if int(i) < 256 and int(i) > -1:
-                   # print("valid octet: " + a)
                     pass
                 else:
                     return("invalid ip octet: " + a)
@@ -33,21 +33,12 @@ def verifyOctet(a):
         return("Invalid no of octet:" + a)
         sys.exit()
 
-def verifyIp(x):
-    #with open(x,"r")  as f:
-        #jsondata = json.loads(f.read())
-        #print(json.dumps(jsondata, indent=2))
-        #for devices in jsondata:
-            #print(jsondata[devices]["ip"])
-            #a= jsondata[devices]["ip"]
-            #verifyOctet(a)
-    a=verifyOctet(x)
-    return(a)
+# To add ip_extractor from json/yaml device file soon
 
+'''
 
+#test:
+a = verifyIpOctet("192.168.1.1")
+print(a)
 
-
-
-
-
-#verifyIp(file)
+'''
